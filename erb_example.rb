20b8c674fe5e2da5eff24df56108645ea88ca170
Anwesh <anwesh@anweshknayak@gmail.com>
2015-06-17 16:09:33 +0530

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -2,4 +2,10 @@ require 'erb'
 
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
+y = 'staafa'
+template1 = ERB.new "The value of whatva is: <%= y%>"
+z = ['a','b','c']
+template2 = ERB.new "The value of whatva is: <%= z%>"
 puts template.result(binding)
+puts template1.result(binding)
+puts template2.result(binding)
